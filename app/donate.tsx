3ff01d898855d9640579076347dc4b643d6b2a45
Reinier Hernández <sasuke.reinier@gmail.com>
2024-10-08 14:16:06 -0300

    feat(donate-section): add SWIFT information and copy to clipboard functionality

--- app/donate.tsx
+++ app/donate.tsx
@@ -1,20 +1,45 @@
-import { ThemedText } from "@/components/themed";
-import SimpleLayout from "@/components/layouts/SimpleLayout";
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
+import * as Clipboard from 'expo-clipboard';
+import { ThemedText } from "@/components/themed";
+import SimpleLayout from "@/components/layouts/SimpleLayout";
 
 export default function DonateScreen() {
   const { t } = useTranslation();
   const [selectedCurrency, setSelectedCurrency] = useState<'USD' | 'SRD'>('USD');
 
+  const swiftInfo = {
+    USD: {
+      bank: 'JP Morgan Chase Bank',
+      accountName: 'John Doe',
+      accountNumber: '123456789',
+      swiftCode: 'CHASUS33',
+    },
+    SRD: {
+      bank: 'Republic Bank',
+      accountName: 'John Doe',
+      accountNumber: '123456789',
+      swiftCode: 'RBNKSRPA',
+    },
+  }
+
+  const copyToClipboard = (text: string) => {
+    Clipboard.setString(text);
+  }
+
   return (
     <SimpleLayout style={styles.container}>
       <ThemedText type="title">Donate</ThemedText>
       <ThemedText type="subtitle">Support Our App</ThemedText>
-      <ThemedText>Your donations help us improve the app and keep it ad-free. Use the SWIFT information bellow to make a donation:</ThemedText>
-      <ThemedText>Your donation is greatly appreciated. All proceeds go directly towards app development and subscriptions costs.</ThemedText>
+      <ThemedText style={styles.textMuted}>Your donations help us improve the app and keep it ad-free. Use the SWIFT information bellow to make a donation:</ThemedText>
 
+      <View>
+        <View style={styles.selector}>
+        </View>
+      </View>
+
+      <ThemedText style={styles.textMuted}>Your donation is greatly appreciated. All proceeds go directly towards app development and subscriptions costs.</ThemedText>
     </SimpleLayout>
   )
 }
@@ -36,4 +61,13 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.1)',
     marginVertical: 12,
   },
+  textMuted: {
+    color: 'gray',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  selector: {
+    height: 40,
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  }
 });
